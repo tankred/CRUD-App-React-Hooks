bdd3874b7cdf27e6f8022955e539387a25ff53d8
Kurt Duyck <kurt.duyck@vives.be>
2021-01-13 13:18:42 +0100

    Add array of users
    	modified:   CHANGELOG.md
    	modified:   src/App.js
    	modified:   src/tables/UserTable.js

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.5.3] - 2021-01-13
+### Changed
+- Import data 
+
 ## [0.5.2] - 2021-01-12
 ### Added
 - User table
--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import UserTable from './tables/UserTable'
 
 const App = () => {
+  const usersData = [
+    { id: 1, name: 'Tania', username: 'floppydiskette' },
+    { id: 2, name: 'Craig', username: 'siliconeidolon' },
+    { id: 3, name: 'Ben', username: 'benisphere' },
+  ]
+
+  // eslint-disable-next-line
+  const [users, setUsers] = useState(usersData)
+
   return (
     <div className="container">
       <h1>CRUD App with Hooks</h1>
@@ -11,7 +20,7 @@ const App = () => {
         </div>
         <div className="flex-large">
           <h2>View users</h2>
-          <UserTable />
+    	   <UserTable users={users} />
         </div>
       </div>
     </div>
